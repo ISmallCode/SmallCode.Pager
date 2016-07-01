f5c165d092fca4bfc08d71dd84fec835fcb6701d
applenele <1941973283@qq.com>
2016-07-01 22:36:59 +0800

    rtm

--- SmallCode.Pager/src/SmallCode.Pager/obj/Debug/netstandard1.6/dotnet-compile.assemblyinfo.cs
+++ SmallCode.Pager/src/SmallCode.Pager/obj/Debug/netstandard1.6/dotnet-compile.assemblyinfo.cs
@@ -5,4 +5,4 @@
 [assembly:System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly:System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly:System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly:System.Runtime.Versioning.TargetFrameworkAttribute(".NETStandard,Version=v1.5")]
+[assembly:System.Runtime.Versioning.TargetFrameworkAttribute(".NETStandard,Version=v1.6")]
